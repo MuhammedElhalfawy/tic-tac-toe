c7704e23867a2dd7449f7fd34229d434a9405dc9
Muhammed Elhalfawy <mohamedelhalfawy02@gmail.com>
2022-12-17 12:19:05 +0200

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor =  Color(0xFFff4b4b);
+  static Color secondColor =  Color(0xFFffca77);
+  static Color accentColor =  Color(0xFF4169e8);
+}
